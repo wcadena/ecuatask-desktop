5225fa299c8752fa840f40ca800a040b2e6279b6
wcadena <wcadena@outlook.com>
2019-11-28 12:19:37 -0500

    Add sass

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+// include all css files
+import './lib/VuelyCss'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
