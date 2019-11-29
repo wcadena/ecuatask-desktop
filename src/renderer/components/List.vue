681e4d12bcb3887d74727782095282b1a1ca6773
wcadena <wcadena@outlook.com>
2019-11-29 08:41:50 -0500

    Se arregla nueva version de vuetify en list

--- src/renderer/components/List.vue
+++ src/renderer/components/List.vue
@@ -2,15 +2,15 @@
   <v-list two-line>
     <template v-for="item in data">
       <v-divider></v-divider>
-      <v-list-tile avatar>
-        <v-list-tile-avatar v-if="item.img">
+      <v-list-item avatar>
+        <v-list-item-avatar v-if="item.img">
           <img :src="item.img" />
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title>{{item.name}}</v-list-tile-title>
-          <v-list-tile-sub-title>{{item.value}} {{(item.unit) ? item.unit : ''}}</v-list-tile-sub-title>
-        </v-list-tile-content>
-      </v-list-tile>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{item.name}}</v-list-item-title>
+          <v-list-item-subtitle>{{item.value}} {{(item.unit) ? item.unit : ''}}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
     </template>
   </v-list>
 </template>
