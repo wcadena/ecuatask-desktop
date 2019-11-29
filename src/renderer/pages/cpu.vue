a6d367e48def3c31a522b5f76acba1898909f68d
wcadena <wcadena@outlook.com>
2019-11-29 08:38:04 -0500

    se aumenta try catch asincronico

--- src/renderer/pages/cpu.vue
+++ src/renderer/pages/cpu.vue
@@ -6,56 +6,59 @@
 
 <script>
 import List from '../components/List'
-import store from '../store/index'
-store.dispatch('SET_TITLE', 'System Infomation')
+
 export default {
   components: {
     List
   },
   computed: {
     data () {
-      return [
-        {
-          name: 'Brand',
-          value: this.$store.state.data.cpu.brand,
-          img: `static/cpu/cpu.svg`
-        },
-        {
-          name: 'Cores',
-          value: this.$store.state.data.cpu.cores,
-          img: `static/cpu/core.svg`
-        },
-        {
-          name: 'Manufacturer',
-          value: this.$store.state.data.cpu.manufacturer,
-          img: `static/cpu/manufacturer.svg`
-        },
-        {
-          name: 'Model',
-          value: this.$store.state.data.cpu.model,
-          img: `static/cpu/model.svg`
-        },
-        {
-          name: 'Revision',
-          value: this.$store.state.data.cpu.revision,
-          img: `static/cpu/revision.svg`
-        },
-        {
-          name: 'Speed',
-          value: this.$store.state.data.cpu.speed,
-          img: `static/cpu/speed.svg`
-        },
-        {
-          name: 'Speed Min',
-          value: this.$store.state.data.cpu.speedmin,
-          img: `static/cpu/speedmin.svg`
-        },
-        {
-          name: 'Speed Max',
-          value: this.$store.state.data.cpu.speedmax,
-          img: `static/cpu/speedmax.svg`
-        }
-      ]
+      try {
+        return [
+          {
+            name: 'Brand',
+            value: this.$store.state.data.cpu.brand,
+            img: `static/cpu/cpu.svg`
+          },
+          {
+            name: 'Cores',
+            value: this.$store.state.data.cpu.cores,
+            img: `static/cpu/core.svg`
+          },
+          {
+            name: 'Manufacturer',
+            value: this.$store.state.data.cpu.manufacturer,
+            img: `static/cpu/manufacturer.svg`
+          },
+          {
+            name: 'Model',
+            value: this.$store.state.data.cpu.model,
+            img: `static/cpu/model.svg`
+          },
+          {
+            name: 'Revision',
+            value: this.$store.state.data.cpu.revision,
+            img: `static/cpu/revision.svg`
+          },
+          {
+            name: 'Speed',
+            value: this.$store.state.data.cpu.speed,
+            img: `static/cpu/speed.svg`
+          },
+          {
+            name: 'Speed Min',
+            value: this.$store.state.data.cpu.speedmin,
+            img: `static/cpu/speedmin.svg`
+          },
+          {
+            name: 'Speed Max',
+            value: this.$store.state.data.cpu.speedmax,
+            img: `static/cpu/speedmax.svg`
+          }
+        ]
+      } catch (e) {
+        return []
+      }
     }
   }
 }
