56dcdaf166a6f53ab42df99c9045dc49883ac04e
wcadena <wcadena@outlook.com>
2019-12-10 09:09:25 -0500

    Se da un fix a redes

--- src/renderer/pages/network.vue
+++ src/renderer/pages/network.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <list :data="data" />
+    <v-expansion-panels>
+      <v-expansion-panel
+              v-for="(position, index) in $store.state.data.net.length"
+              :key="index"
+      >
+        <v-expansion-panel-header disable-icon-rotate >
+          {{$store.state.data.net[index].ifaceName}}<span v-if="$store.state.data.networkStats[0].iface.toLowerCase() == $store.state.data.net[index].iface.toLowerCase()">Tarjeta Activa</span>
+          <template v-slot:actions>
+            <v-icon color="teal">mdi-check</v-icon>
+          </template>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <list :data="getDiskData(index)" />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 
@@ -10,34 +25,34 @@ export default {
   components: {
     List
   },
-  computed: {
-    data () {
-      let iface = this.$store.state.data.networkStats[0].iface
-      let net = this.$store.state.data.net.filter(
-        data => data.iface === iface
-      )[0]
-      return [
-        {
-          name: 'Name',
-          value: net.iface,
-          img: require(`@/static/network/name.svg`)
-        },
-        {
-          name: 'IP v4',
-          value: net.ip4,
-          img: require(`@/static/network/ip4.svg`)
-        },
-        {
-          name: 'IP v6',
-          value: net.ip6,
-          img: require(`@/static/network/ip6.svg`)
-        },
-        {
-          name: 'MAC',
-          value: net.mac,
-          img: require(`@/static/network/mac.svg`)
-        }
-      ]
+  methods: {
+    getDiskData (num) {
+      try {
+        return [
+          {
+            name: 'Name',
+            value: this.$store.state.data.net[num].iface,
+            img: require(`@/static/network/name.svg`)
+          },
+          {
+            name: 'IP v4',
+            value: this.$store.state.data.net[num].ip4,
+            img: require(`@/static/network/ip4.svg`)
+          },
+          {
+            name: 'IP v6',
+            value: this.$store.state.data.net[num].ip6,
+            img: require(`@/static/network/ip6.svg`)
+          },
+          {
+            name: 'MAC',
+            value: this.$store.state.data.net[num].mac,
+            img: require(`@/static/network/mac.svg`)
+          }
+        ]
+      } catch (e) {
+        return []
+      }
     }
   }
 }
