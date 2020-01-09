b4f17814b9cc8b5f095a87396dd2d5a35ee85494
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2020-01-08 22:16:54 -0500

    se regresa a opciones coneccion global

--- src/renderer/oauth/auth0-variables.js
+++ src/renderer/oauth/auth0-variables.js
@@ -1,12 +1,12 @@
 export const AUTH_CONFIG = {
-  clientId: 1,
-  clientSecret: 'qcbDnG3QeZ2Wr7SBViDFKKxF2vFj0jOECG8MfhHX',
-  domain: 'http://inventarios.local/oauth/token',
-  apibase: 'http://inventarios.local',
+  clientId: 8,
+  clientSecret: 'gWKflHgHzlKPX9FQpVZmgPDzv60iXnKujzRyzwVV',
+  domain: 'https://devinventario.ecuatask.com/oauth/token',
+  apibase: 'https://devinventario.ecuatask.com',
   callbackUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/callback' : 'http://vuely.theironnetwork.org/callback',
   apiUrl: 'API_IDENTIFIER',
   grantType: 'password',
   username: 'admin@admin.com',
-  password: 'admin',
+  password: 'password',
   scope: '*'
 }
