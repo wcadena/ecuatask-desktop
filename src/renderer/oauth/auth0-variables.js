97831e1e7746fa2c84068decd443affa76e351c2
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2020-01-09 22:22:30 -0500

    se actualiza credenciales para pruebas

--- src/renderer/oauth/auth0-variables.js
+++ src/renderer/oauth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  clientId: 8,
-  clientSecret: 'gWKflHgHzlKPX9FQpVZmgPDzv60iXnKujzRyzwVV',
+  clientId: 1,
+  clientSecret: 'sJxSKRoudJcr4jMS1gIRqHARlxTwZQiiAf0EqyO3',
   domain: 'https://devinventario.ecuatask.com/oauth/token',
   apibase: 'https://devinventario.ecuatask.com',
   callbackUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/callback' : 'http://vuely.theironnetwork.org/callback',
